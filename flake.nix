508581eeb2d6adf8359dfd14014cc5db347bd39a
ymeister <47071325+ymeister@users.noreply.github.com>
2025-08-06 16:01:34 +0400

    Provide substiters in 'flake.nix'

--- flake.nix
+++ flake.nix
@@ -13,4 +13,16 @@
         packages.manualMarkdown = (project { src = ./.; }).manualMarkdown;
       }
     );
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nixcache.reflex-frp.org"
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" # reflex-frp
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    allow-import-from-derivation = "true";
+  };
 }
